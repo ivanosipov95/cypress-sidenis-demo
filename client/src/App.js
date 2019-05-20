6b7975233d1d0e6cc1915303e1fe711b907827f0
Ivan Osipov <extern.osipov_ivan@allianz.com>
2019-05-20 23:11:52 +0300

    integration with BE was done

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function testBackend() {
+        fetch('/api/test')
+            .then((res) => console.log(res));
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo}
+                     className="App-logo"
+                     alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+
+                <button onClick={testBackend}>Test BE</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4200;
+
 app.get('/api/test', (req, res) => res.json({text: 'is tested'}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
