95f3e5ff1dddbe77fd71250c2571cf79e0e62d73
orion <crissorion@gmail.com>
2019-05-22 16:53:26 +0300

    - calendar stub table

--- client/src/components/CalendarEditor.js
+++ client/src/components/CalendarEditor.js
@@ -1,3 +1,32 @@
 import React from 'react'
+import { Grid, Paper, Table, TableBody, TableCell, TableRow } from '@material-ui/core';
 
-export default () => <div></div>
+const rows = [
+    { id: 1, name: 'test', description: 'hello world' },
+    { id: 2, name: 'test', description: 'hello world' },
+    { id: 3, name: 'test', description: 'hello world' },
+    { id: 4, name: 'test', description: 'hello world' },
+];
+
+export default () =>
+    <Grid container>
+        <Paper style={{ width: '100%' }}>
+            <Table>
+                <TableBody>
+                    {rows.map(row => (
+                        <TableRow key={row.id}>
+                            <TableCell>
+                                {row.id}
+                            </TableCell>
+                            <TableCell>
+                                {row.name}
+                            </TableCell>
+                            <TableCell>
+                                {row.description}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Paper>
+    </Grid>
