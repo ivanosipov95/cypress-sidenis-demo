e6b230f2e7e08ee9f5613dcf188136ebea2167e3
Sergey Ivanov <sergey.ivanov@sidenis.com>
2019-05-24 15:03:44 +0300

    - timer improved

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Button, LinearProgress } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import { useInterval } from "../utils/hooks";
+import { formatTime } from "../utils/time";
 
 export default ({ onStop }) => {
     const [value, setValue] = useState(0);
@@ -14,12 +15,17 @@ export default ({ onStop }) => {
     };
 
     return (
-        <Grid container alignItems='center'>
-            <Grid item xs>
-                <LinearProgress variant='determinate' value={progress}/>
+        <Grid container direction='column'>
+            <Grid item>
+                {isOn && formatTime(value)}
             </Grid>
-            <Grid item xs>
-                <Button onClick={toggleTimer} data-test='timer-btn'>{isOn ? 'Stop' : 'Start'}</Button>
+            <Grid container alignItems='center'>
+                <Grid item xs>
+                    <LinearProgress variant='determinate' value={progress}/>
+                </Grid>
+                <Grid item>
+                    <Button onClick={toggleTimer} data-test='timer-btn'>{isOn ? 'Stop' : 'Start'}</Button>
+                </Grid>
             </Grid>
         </Grid>
     )
--- client/src/utils/time.js
+++ client/src/utils/time.js
@@ -1,8 +1,14 @@
 export function formatTime(totalInSec) {
-    const seconds = totalInSec % 60;
+    const seconds = twoDigitNum(totalInSec % 60);
     const totalMinutes = parseInt(totalInSec / 60);
-    const minutes = totalMinutes % 60;
-    const hours = parseInt(totalMinutes / 60);
+    const minutes = twoDigitNum(totalMinutes % 60);
+    const hours = twoDigitNum(parseInt(totalMinutes / 60));
 
     return `${hours}:${minutes}:${seconds}`;
+}
+
+function twoDigitNum(val) {
+    const str = '' + val;
+
+    return str.length == 1 ? '0' + str : str;
 }
