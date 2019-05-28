a1137643062e517986a85bb7174f9495774973f4
Ivan Osipov <extern.osipov_ivan@allianz.com>
2019-05-28 23:46:40 +0300

    Add several notes and new demo file

--- cypress/integration/timemaster.spec.js
+++ cypress/integration/timemaster.spec.js
@@ -1,42 +1,40 @@
 describe('Timemaster', () => {
 
-    it('should display user name', () => {
-        cy.visit('/');
-
-
-        //TODO: bdd assertions - show on original site, talk about tool a bit
-        cy.get('[data-test=user-name]')
-            .should('contain', 'Vasiliy Puaaki')
+    beforeEach(() => {
+        cy.visit('http://localhost:3000');
     });
 
-    it.only('should add history row', () => {
+    it.only('should display user name', () => {
+        cy.get('[data-test=user-name]')
+            .contains('Vasiliy Pupkin');
+    });
+
+    it('should add history row', () => {
         cy.visit('/');
 
         startTimer();
         stopTimer();
 
-        addDescription('test desc')
+        addDescription('test desc');
 
         cy.get('[data-test=history-row]')
             .contains('[data-test=description]', 'test desc')
             .should('exist');
-    })
-
+    });
 
     it('it does not append new line', () => {
 
-    })
+    });
 });
-
 
 function startTimer() {
     cy.get('[data-test=timer-btn]')
-        .click()
+        .click();
 }
 
 function stopTimer() {
     cy.get('[data-test=timer-btn]')
-        .click()
+        .click();
 }
 
 function addDescription(description) {
@@ -45,6 +43,5 @@ function addDescription(description) {
         .blur();
 
     cy.get('[data-test=accept]')
-        .click()
+        .click();
 }
-
